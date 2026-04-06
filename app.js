6f15eb740a7a23834d8e4acfd6aa13887488c21d
GABRIEL SOUSA RODRIGUES DE MELO <25203630@CFP127ALUNO.LOCAL>
2026-04-06 09:38:56 -0300

    Ste finalizado

--- app.js
+++ app.js
@@ -2,3 +2,23 @@
 
 import funcionarios from "./funcionarios.json" with { type: "json" }
 
+function criarCard(funcionarios){
+    const card = document.createElement('div')
+    card.className = 'card'
+
+    const foto = document.createElement('img')
+    foto.src = `./img/${funcionarios.imagem}`
+
+    const nome = document.createElement('h3')
+    nome.textContent = funcionarios.nome
+
+    const cargo = document.createElement('span')
+    cargo.textContent = funcionarios.cargo
+
+    card.append(foto, nome, cargo)
+
+    return card
+}
+const cards = funcionarios.map(criarCard)
+
+document.getElementById('container').replaceChildren(...cards)
